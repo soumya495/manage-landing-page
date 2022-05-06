013554fd427576a10e46852b192572aa6321d689
Soumya Banerjee <soumyabanerjee783@gmail.com>
2022-05-06 15:34:15 +0530

    Hamburger menu and some style changes

--- js/index.js
+++ js/index.js
@@ -27,11 +27,38 @@ var swiper = new Swiper('.mySwiper', {
   },
 })
 
-let paginationSpans = document.querySelectorAll('.swiper-pagination span')
+let gradClass =
+  'w-screen h-screen bg-gradient-to-br from-gray-700 via-gray-900 to-black  overflow-y-hidden'
 
-paginationSpans.forEach((pSpan) => {
-  pSpan.style.border = '2px solid hsl(12, 88%, 59%)'
-  pSpan.style.width = '0.65rem'
-  pSpan.style.height = '0.65rem'
-  pSpan.style.backgroundColor = 'hsl(12, 88%, 59%)'
+let hamburgerIcon = document.querySelector('.ham-menu')
+let hamLines = document.querySelectorAll('.ham-menu div')
+let mobMenu = document.querySelector('.mobile-menu')
+let allSections = document.querySelectorAll('section')
+console.log(allSections)
+
+hamburgerIcon.addEventListener('click', function () {
+  if (!hamburgerIcon.classList.contains('open')) {
+    hamLines[0].style.transform = 'rotate(-45deg) translateY(5px)'
+    hamLines[2].style.transform = 'rotate(45deg) translateY(-5px)'
+    hamLines[1].style.position = 'absolute'
+    hamLines[1].style.opacity = '0'
+    hamburgerIcon.classList.add('open')
+    hamburgerIcon.classList.add('open')
+    document.body.style.overflowY = 'hidden'
+    document.body.style.backgroundImage =
+      'linear-gradient(rgb(255,255,255), rgb(156, 163, 175), rgb(75, 85, 99))'
+    allSections.forEach((sec) => (sec.style.filter = 'blur(10px)'))
+    mobMenu.style.display = 'flex'
+  } else {
+    hamLines[0].style.transform = 'rotate(0) translateY(0)'
+    hamLines[2].style.transform = 'rotate(0) translateY(0)'
+    hamLines[1].style.opacity = '1'
+    hamLines[1].style.position = 'unset'
+    hamburgerIcon.classList.remove('open')
+    mobMenu.style.display = 'none'
+    document.body.style.overflowY = 'scroll'
+    document.body.style.backgroundImage = 'unset'
+    allSections.forEach((sec) => (sec.style.filter = 'unset'))
+    mobMenu.style.display = 'none'
+  }
 })
